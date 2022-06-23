36f1be9965835707e66a5bcb723683180b03b42b
Matthew Ng <theisose@gmail.com>
2022-06-23 09:16:43 -0700

    Created sequelize models for questions and tags

---
+++ backend/models/questionTagMap.js
@@ -0,0 +1,46 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class questionTagMap extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  questionTagMap.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      questionId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'question',
+          key: 'question_id',
+        },
+      },
+      tagId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+          model: 'tag',
+          key: 'tag',
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'question_tag_map',
+      tableName: 'question_tag_map',
+      underscored: true,
+    },
+  )
+  return questionTagMap
+}
---
+++ backend/models/tag.js
@@ -0,0 +1,35 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class tag extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  tag.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      tag: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'tag',
+      tableName: 'tag',
+      underscored: true,
+    },
+  )
+  return tag
+}
