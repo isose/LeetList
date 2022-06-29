f0c8f2bee91db6cb958d97dbd5498a3ad78d6fcb
Matthew Ng <theisose@gmail.com>
2022-06-28 19:42:11 -0700

    Created question endpoint

--- backend/models/question.js
+++ backend/models/question.js
@@ -10,6 +10,10 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
+
+    toJSON() {
+      return { ...this.get(), id: undefined, createdAt: undefined, updatedAt: undefined };
+    }
   }
   question.init(
     {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,22 +7,6 @@ class App extends Component {
     data: null,
   };
 
-  componentDidMount() {
-    this.callBackendAPI()
-      .then((res) => this.setState({ data: res.express }))
-      .catch((err) => console.log(err));
-  }
-  // fetching the GET route from the Express server which matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message);
-    }
-    return body;
-  };
-
   render() {
     return (
       <div className='App'>
