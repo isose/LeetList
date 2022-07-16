86f2a849c9f1cea81a513b7e9ad07763a36b6c68
Matthew Ng <theisose@gmail.com>
2022-07-16 03:47:06 -0700

    Implemented tag dropdown

--- frontend/components/search/TagsDropdown.tsx
+++ frontend/components/search/TagsDropdown.tsx
@@ -4,6 +4,7 @@ import Tag from '../Tag';
 const TagsDropdown = ({ selected, setSelected }: any) => {
   const [open, setOpen] = useState(false);
   const [tags, setTags] = useState([]);
+  const [tagFilter, setTagFilter] = useState('');
 
   useEffect(() => {
     getTags();
@@ -39,24 +40,37 @@ const TagsDropdown = ({ selected, setSelected }: any) => {
 
   document.addEventListener('mousedown', clickOutside);
 
-  // TODO add tags to query
-  // TODO add switch for logical AND or logical OR
-  // TODO add clear tags button
-  // TODO search bar for tags container
-  // TODO scrollbar for tags container
-
   return (
     <div className='tags-dropdown-wrapper' ref={tagsDropdown}>
-      <button className='tags-dropdown-header' onClick={() => toggleState()}>
+      <button className='tags-dropdown-button' onClick={() => toggleState()}>
         Tags
       </button>
       {open && (
-        <div className='tags-dropdown-container tag-list'>
-          {tags.map((tag: any, index: number) => {
-            return (
-              <Tag key={index} tag={tag} onClick={handleClick} styleName={getStyleName(tag)} />
-            );
-          })}
+        <div className='tags-dropdown-container'>
+          <div className='tags-dropdown-header'>
+            <input
+              className='search-bar'
+              placeholder='Filter tags'
+              value={tagFilter}
+              onInput={(e) => setTagFilter((e.target as HTMLInputElement).value)}
+            ></input>
+          </div>
+          <div className='tag-list'>
+            {tags
+              .filter((tag: any) => {
+                return tag.tagName.toLowerCase().indexOf(tagFilter.toLowerCase()) !== -1;
+              })
+              .map((tag: any, index: number) => {
+                return (
+                  <Tag key={index} tag={tag} onClick={handleClick} styleName={getStyleName(tag)} />
+                );
+              })}
+          </div>
+          <div className='tags-dropdown-footer'>
+            <button className='reset-tags-button' onClick={() => setSelected([])}>
+              Reset Tags
+            </button>
+          </div>
         </div>
       )}
     </div>
