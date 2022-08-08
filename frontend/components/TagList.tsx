606002ce5b8f163672c52bd750fec1ec35242e6b
Matthew Ng <theisose@gmail.com>
2022-08-08 03:53:27 -0700

    Hide tags and add more button when tags overflow

--- frontend/components/TagList.tsx
+++ frontend/components/TagList.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Tag from './Tag';
 
 const TagList = ({ tags }: any) => {
+  const [isOverflowed, setIsOverflowed] = useState(false);
+  const [expanded, setExpanded] = useState(false);
+  const elementRef = useRef<any>();
+
+  const checkOverflow = () => {
+    const overflow = elementRef.current.scrollHeight > elementRef.current.clientHeight;
+    setIsOverflowed(overflow);
+  };
+
+  useEffect(() => {
+    checkOverflow();
+    window.addEventListener('resize', checkOverflow);
+  }, []);
+
+  useEffect(
+    () => () => {
+      window.removeEventListener('resize', checkOverflow);
+    },
+    [],
+  );
+
   return (
-    <div className='tag-list'>
-      {tags.map((tag: any, index: number) => {
-        return <Tag key={index} tag={tag} />;
-      })}
+    <div className='tag-list-wrapper'>
+      <div className={`tag-list${expanded ? ' expanded' : ''}`} ref={elementRef}>
+        {tags.map((tag: any, index: number) => {
+          return <Tag key={index} tag={tag} />;
+        })}
+      </div>
+      {isOverflowed && !expanded && (
+        <span className='expand-tags' onClick={() => setExpanded(true)}>
+          more
+        </span>
+      )}
     </div>
   );
 };
